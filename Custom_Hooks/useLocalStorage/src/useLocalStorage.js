8d9c23610a561af70031fe112f5b64c2044bcaf7
souvik-pl <souvik.9401@gmail.com>
2024-12-13 09:23:31 +0530

    minor fix in useLocalStorage

--- Custom_Hooks/useLocalStorage/src/useLocalStorage.js
+++ Custom_Hooks/useLocalStorage/src/useLocalStorage.js
@@ -1,17 +1,18 @@
 import { useEffect, useState } from "react";
 
-function getSavedValue(key, initialValue) {
-  const savedValue = JSON.parse(localStorage.getItem(key));
-  if (savedValue) {
-    return savedValue;
-  }
-
-  if (initialValue instanceof Function) return initialValue();
-  return initialValue;
-}
-
 export const useLocalStorage = (key, initialValue) => {
-  const [value, setValue] = useState(() => getSavedValue(key, initialValue));
+  const [value, setValue] = useState(() => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue) {
+      return JSON.parse(savedValue);
+    }
+
+    if (typeof data === "function") {
+      return data();
+    }
+
+    return data;
+  });
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
