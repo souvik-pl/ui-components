7ec9566354cba64341dc5a07d685cc14d7c614fe
souvik-pl <souvik.9401@gmail.com>
2024-12-13 22:18:40 +0530

    useThrottle: added another way

--- Custom_Hooks/useThrottle/src/useThrottle.js
+++ Custom_Hooks/useThrottle/src/useThrottle.js
@@ -1,5 +1,26 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Can be done like this as well 👇
+ */
+
+// const useThrottle = (input, delay) => {
+//   const [value, setValue] = useState(input);
+//   const isAllowed = useRef(true);
+
+//   useEffect(() => {
+//     if (isAllowed.current) {
+//       setValue(input);
+//       isAllowed.current = false;
+//       setTimeout(() => {
+//         isAllowed.current = true;
+//       }, delay || 400);
+//     }
+//   }, [input]);
+
+//   return value;
+// };
+
 const useThrottle = (query, delay) => {
   const [throttledQuery, setThrottledQuery] = useState("");
   const [isQuerySettingAllowed, setIsQuerySettingAllowed] = useState(true);
