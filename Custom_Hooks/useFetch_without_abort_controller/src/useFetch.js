14886b998bb1ed43e2effac572cab3d86edbfcd6
souvik-pl <souvik.9401@gmail.com>
2025-07-23 22:05:20 +0530

    useFetch without abort controller

---
+++ Custom_Hooks/useFetch_without_abort_controller/src/useFetch.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        if (isMounted) setData(data);
+      } catch (error) {
+        if (isMounted) setError(error);
+      } finally {
+        if (isMounted) setLoading(false);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [url]);
+
+  return {
+    data,
+    loading,
+    error,
+  };
+};
+
+export default useFetch;
